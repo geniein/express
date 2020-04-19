b0771647cc0cf30b9afad476fb4d452354645455
ingenie <cubya0104@gmail.com>
2020-04-19 12:07:34 +0900

    init 6 Write

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,239 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {Write} from 'components';
+import {Write, MemoList} from 'components';
+import { memoPostRequest, memoListRequest} from 'actions/memo';
+
 
 class Home extends React.Component {
+
+    constructor(props) {
+        super(props);        
+        this.handlePost = this.handlePost.bind(this);
+        this.loadNewMemo = this.loadNewMemo.bind(this);
+        this.state = {
+            loadingState: false
+        };
+    }
+
+    componentDidMount() {
+         // LOAD NEW MEMO EVERY 5 SECONDS
+         const loadMemoLoop = () => {
+            this.loadNewMemo().then(
+                () => {
+                    this.memoLoaderTimeoutId = setTimeout(loadMemoLoop, 5000);
+                }
+            );
+        };
+        this.props.memoListRequest(true).then(
+            () => {
+                loadMemoLoop();
+            }
+        );
+
+        const loadUntilScrollable = () => {
+            // IF THE SCROLLBAR DOES NOT EXIST,
+            if($("body").height() < $(window).height()) {
+                this.loadOldMemo().then(
+                    () => {
+                        // DO THIS RECURSIVELY UNLESS IT'S LAST PAGE
+                        if(!this.props.isLast) {
+                            loadUntilScrollable();
+                        }
+                    }
+                );
+            }
+        };
+        
+        this.props.memoListRequest(true).then(
+            () => {
+                // BEGIN NEW MEMO LOADING LOOP
+                loadUntilScrollable();
+                loadMemoLoop();
+            }
+        );
+
+        $(window).scroll(() => {
+            // WHEN HEIGHT UNDER SCROLLBOTTOM IS LESS THEN 250
+            if ($(document).height() - $(window).height() - $(window).scrollTop() < 250) {
+                if(!this.state.loadingState){
+                    console.log("LOAD NOW");
+                    this.loadOldMemo();
+                    this.setState({
+                        loadingState: true
+                    });
+                }
+            } else {
+                if(this.state.loadingState){
+                    this.setState({
+                        loadingState: false
+                    });
+                }
+            }
+        });
+    }
+    componentWillUnmount() {
+        // STOPS THE loadMemoLoop
+        clearTimeout(this.memoLoaderTimeoutId);
+         // REMOVE WINDOWS SCROLL LISTENER
+         $(window).unbind();
+    }
+    loadNewMemo() {
+        // CANCEL IF THERE IS A PENDING REQUEST
+        if(this.props.listStatus === 'WAITING') 
+            return new Promise((resolve, reject)=> {
+                resolve();
+            });
+        
+        // IF PAGE IS EMPTY, DO THE INITIAL LOADING
+        if(this.props.memoData.length === 0 )
+            return this.props.memoListRequest(true);
+            
+        return this.props.memoListRequest(false, 'new', this.props.memoData[0]._id);
+    }
+    loadOldMemo() {
+        // CANCEL IF USER IS READING THE LAST PAGE
+        if(this.props.isLast) {
+            return new Promise(
+                (resolve, reject)=> {
+                    resolve();
+                }
+            );
+        }
+        
+        // GET ID OF THE MEMO AT THE BOTTOM
+        let lastId = this.props.memoData[this.props.memoData.length - 1]._id;
+        
+        // START REQUEST
+        return this.props.memoListRequest(false, 'old', lastId).then(() => {
+            // IF IT IS LAST PAGE, NOTIFY
+            if(this.props.isLast) {
+                Materialize.toast('You are reading the last page', 2000);
+            }
+        });
+    }
+
+    /* POST MEMO */
+    handlePost(contents) {
+        return this.props.memoPostRequest(contents).then(
+            () => {
+                if(this.props.postStatus.status === "SUCCESS") {
+                    // TRIGGER LOAD NEW MEMO
+                    // TO BE IMPLEMENTED
+                    this.loadNewMemo().then(
+                        () => {
+                            Materialize.toast('Success!', 2000);
+                        }
+                    );
+                } else {
+                    /*
+                        ERROR CODES
+                            1: NOT LOGGED IN
+                            2: EMPTY CONTENTS
+                    */
+                    let $toastContent;
+                    switch(this.props.postStatus.error) {
+                        case 1:
+                            // IF NOT LOGGED IN, NOTIFY AND REFRESH AFTER
+                            $toastContent = $('<span style="color: #FFB4BA">You are not logged in</span>');
+                            Materialize.toast($toastContent, 2000);
+                            setTimeout(()=> {location.reload(false);}, 2000);
+                            break;
+                        case 2:
+                            $toastContent = $('<span style="color: #FFB4BA">Please write something</span>');
+                            Materialize.toast($toastContent, 2000);
+                            break;
+                        default:
+                            $toastContent = $('<span style="color: #FFB4BA">Something Broke</span>');
+                            Materialize.toast($toastContent, 2000);
+                            break;
+                    }
+                }
+            }
+        );
+    }
     render() {
-        const write = (<Write/>);
+        const write = (<Write onPost={this.handlePost}/>);
+        //TEST Data Start
+        var testData = [
+            {
+                "_id": "578b958ec1da760909c263f4",
+                "writer": "ingenie",
+                "contents": "Testing",
+                "__v": 0,
+                "is_edited": false,
+                "date": {
+                    "edited": "2016-07-17T14:26:22.428Z",
+                    "created": "2016-07-17T14:26:22.428Z"
+                },
+                "starred": []
+            },
+            {
+                "_id": "578b957ec1da760909c263f3",
+                "writer": "ingenie",
+                "contents": "Data",
+                "__v": 0,
+                "is_edited": false,
+                "date": {
+                    "edited": "2016-07-17T14:26:06.999Z",
+                    "created": "2016-07-17T14:26:06.999Z"
+                },
+                "starred": []
+            },
+            {
+                "_id": "578b957cc1da760909c263f2",
+                "writer": "ingenie",
+                "contents": "Mock",
+                "__v": 0,
+                "is_edited": false,
+                "date": {
+                    "edited": "2016-07-17T14:26:04.195Z",
+                    "created": "2016-07-17T14:26:04.195Z"
+                },
+                "starred": []
+            },
+            {
+                "_id": "578b9579c1da760909c263f1",
+                "writer": "ingenie",
+                "contents": "Some",
+                "__v": 0,
+                "is_edited": false,
+                "date": {
+                    "edited": "2016-07-17T14:26:01.062Z",
+                    "created": "2016-07-17T14:26:01.062Z"
+                },
+                "starred": []
+            },
+            {
+                "_id": "578b9576c1da760909c263f0",
+                "writer": "ingenie",
+                "contents": "Create",
+                "__v": 0,
+                "is_edited": false,
+                "date": {
+                    "edited": "2016-07-17T14:25:58.619Z",
+                    "created": "2016-07-17T14:25:58.619Z"
+                },
+                "starred": []
+            },
+            {
+                "_id": "578b8c82c1da760909c263ef",
+                "writer": "ingenie",
+                "contents": "blablablal",
+                "__v": 0,
+                "is_edited": false,
+                "date": {
+                    "edited": "2016-07-17T13:47:46.611Z",
+                    "created": "2016-07-17T13:47:46.611Z"
+                },
+                "starred": []
+            }
+        ];
+        //TEST Data End
+
         return (
             <div className="wrapper">
                 {this.props.isLoggedIn ? write : undefined}
+                <MemoList data={this.props.memoData} currentUser={this.props.currentUser}/>
             </div>
         );
     }
@@ -15,8 +241,24 @@ class Home extends React.Component {
 
 const mapStateToProps = (state) => {
     return {
-        isLoggedIn : state.authentication.status.isLoggedIn
+        isLoggedIn : state.authentication.status.isLoggedIn,
+        postStatus: state.memo.post,
+        currentUser: state.authentication.status.currentUser,
+        memoData: state.memo.list.data,
+        listStatus: state.memo.list.status,
+        isLast: state.memo.list.isLast
     };
 };
 
-export default connect(mapStateToProps) (Home);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        memoPostRequest: (contents) => {
+            return dispatch(memoPostRequest(contents));
+        },
+        memoListRequest: (isInitial, listType, id, username) => {
+            return dispatch(memoListRequest(isInitial, listType, id, username));
+        }
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps) (Home);
